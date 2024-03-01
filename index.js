947b849801ae790c580d72d722c81f707666948a
Himanth24 <himanthgodari@gmail.com>
2024-03-01 18:36:55 +0530

    Added all functionalities and finalized

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
-function Function(){
+function displayItems(){
     let text = document.getElementById('search').value;
     let url = `https://api.jikan.moe/v4/anime?q=${text}&sfw`;
+    let items = document.getElementById('items');
+    while(items.firstChild) items.removeChild(items.firstChild);
     console.log(url);
     const apiFetch = fetch(url, {mode : 'cors'});
     const details = apiFetch.then((response) => {
         return response.json();
     })
     details.then((response) => {
-        console.log(response);
+        response.data.forEach(element => {
+
+            let newDiv = document.createElement('div');
+            newDiv.className = 'item';
+
+            let img = document.createElement('img');
+            img.className = 'image';
+            img.src = element.images.jpg.image_url;
+
+            let p = document.createElement('p');
+            p.className = 'para';
+            p.textContent = element.title;
+
+            let link = document.createElement('a');
+            link.className = 'animename';
+            link.href = element.url;
+            link.appendChild(img);
+            link.appendChild(p);
+            newDiv.appendChild(link);
+
+            items.appendChild(newDiv);
+            console.log(element);
+        });
     });
+    
 }
 
